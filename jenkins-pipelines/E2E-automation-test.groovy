ddc039019f56a3621857f5ae6e5cf28be4bcc7ea
timrsfo <timrsfo@gmail.com>
2018-05-08 10:45:49 -0700

    jenknsfile git to timrsfo

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -1,7 +1,7 @@
 #!groovy
 node {
     stage('Git checkout') { // for display purposes
-        git 'https://github.com/BushnevYuri/e2e-automation-pipeline.git'
+        git 'https://github.com/timrsfo/e2e-automation-pipeline.git'
     }
     stage('Smoke') {
         try {
